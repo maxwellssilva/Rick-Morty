6fd88392cf1d887acdc029c2d24effe07b7537dc
Maxwell Silva <ctl_maxsilva@uolinc.com>
2024-04-26 13:58:18 -0400

    Initial Commit

---
+++ Space RM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Space RM
+//
+//  Created by Maxwell da Silva e Silva on 26/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
