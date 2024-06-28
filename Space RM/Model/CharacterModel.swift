92056637ddffb3a67d584f5721e3832d22de62bd
Maxwell Silva <maxwell.ssilvva@gmail.com>
2024-06-28 17:18:40 -0400

    [RM-004] - Adaptações arquivos e criando Service

---
+++ Space RM/Model/CharacterModel.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterModel.swift
+//  Space RM
+//
+//  Created by Maxwell da Silva e Silva on 30/05/24.
+//
+
+import UIKit
+
+struct CharacterModel {
+    //var id: Int
+    var image: String
+    var name: String
+    var specie: String
+    //var gender: String
+    //var status: String
+    //var created: String
+}
